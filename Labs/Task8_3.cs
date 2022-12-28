bf6731e244c684338c3c2becd7e97f89ef5a25d6
Борбенчук Кирилл <borbenchukkirya@yandex.ru>
2022-12-28 23:17:49 +0500

    done

---
+++ Labs/Task8_3.cs
@@ -0,0 +1,40 @@
+﻿namespace Labs
+{
+    internal class Task8_3
+    {
+        private static void Main(string[] args)
+        {
+            List<string> annagramms = new List<string> { "code", "doce", "doce", "framer", "frame", "frmae", "frame", "eoj", "joe" };
+            GetListWithoutAnnagramms(annagramms);
+
+            foreach (var i in annagramms)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static void GetListWithoutAnnagramms(List<string> annagramms)
+        {
+            annagramms.Sort();
+
+            for (int i = 0; i < annagramms.Count; i++)
+            {
+                for (int j = i + 1; j < annagramms.Count; j++)
+                    if (IsAnnagram(annagramms[i], annagramms[j]))
+                        annagramms[j] = " ";
+                if (annagramms[i] == " ")
+                    annagramms.Remove(annagramms[i]);
+            }
+        }
+
+        public static bool IsAnnagram(string one, string two)
+        {
+            var t1 = one.ToCharArray();
+            var t2 = two.ToCharArray();
+
+            Array.Sort(t1);
+            Array.Sort(t2);
+            return t2.SequenceEqual(t1);
+        }
+    }
+}
