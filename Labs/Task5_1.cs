1bf9ed27bd640bc06f7a19c3343b15d1658d65e5
Борбенчук Кирилл <borbenchukkirya@yandex.ru>
2022-12-28 22:56:27 +0500

    done

--- Labs/Task5_1.cs
+++ Labs/Task5_1.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Labs
+﻿namespace Labs
 {
     internal class Task5_1
     {
+        public static void Run()
+        {
+            string previousInput = "0";
+            for (; ; )
+            {
+                string input = Console.ReadLine();
+
+                if (input == "") continue;
+                else if (input == "q") break;
+
+                if (int.TryParse(input, out int result)) Console.WriteLine((char)result);
+                if (Convert.ToDouble(input) == Convert.ToDouble(previousInput)) break;
+
+                previousInput = input;
+            }
+
+        }
     }
 }
